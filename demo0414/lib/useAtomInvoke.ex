be66da46a43140dd79f1eee4e7da88b0e413b35e
Shuzhiyuan <shuzhiyuan02@163.com>
2021-04-14 17:10:46 +0800

    use return attr to invoke map

--- demo0414/lib/useAtomInvoke.ex
+++ demo0414/lib/useAtomInvoke.ex
@@ -8,16 +8,20 @@ defmodule AtomInvoke do
   defmacro invoke_by_atom(func_name) do 
     {func_name,["meta data is use less"],[]}
   end 
+  @name %{name: "benladeng"}
+
   def run do
     IO.inspect(quote do be_invoke() end)
     invoke_by_atom(:haha)
     {:haha,[],[]}
   end
-  
+
+  def func_name(),do: @name
+  # def not_direct_use_attr()  
 
 end
 
 
 AtomInvoke.run()
-
+IO.inspect(AtomInvoke.func_name.name)
 # 宏 和 模式匹
