68ce54f520841dafaade5bcc4515ec0930f24190
Shuzhiyuan <shuzhiyuan02@163.com>
2021-04-15 17:34:40 +0800

    ..

---
+++ demo0415/lib/demo0415.ex
@@ -0,0 +1,18 @@
+defmodule Demo0415 do
+  @moduledoc """
+  Documentation for `Demo0415`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Demo0415.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ demo0415/test/demo0415_test.exs
@@ -0,0 +1,8 @@
+defmodule Demo0415Test do
+  use ExUnit.Case
+  doctest Demo0415
+
+  test "greets the world" do
+    assert Demo0415.hello() == :world
+  end
+end
