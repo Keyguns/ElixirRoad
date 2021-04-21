0ef8d0b39c6570c7583763a4e2ba20d42b593b88
Shuzhiyuan <shuzhiyuan02@163.com>
2021-04-21 10:26:10 +0800

    create LayoutMacro demo and verify name problem

--- layoutmacro/lib/layoutmacro.ex
+++ layoutmacro/lib/layoutmacro.ex
@@ -1,31 +1,51 @@
+# ExUnit.start()
+# use ExUnit.Case, async: true
 defmodule Layoutmacro do
   defmacro layout(module_name,do: clause) do
     {:__block__,[],message_list} = clause
-    Enum.map(message_list,fn message ->
+    messages = Enum.map(message_list,fn message ->
       {_,_,[emotion,status]} = message
-      
+      {emotion,status}
     end)
-    quote do
-      defmodule unquote(module_name) do
 
+    funcs = Enum.map(messages,fn message -> quote do assert message(unquote(message)) end end)
+
+    quote do
+      ExUnit.start()
+      defmodule unquote(module_name) do
+        use ExUnit.Case, async: true
+        unquote (funcs)
       end
     end
   end
 
 
+  def message({emotion,status}) do
+    IO.inspect(emotion<>status)
+  end
+
 end
 
 defmodule Run do
   import Layoutmacro
-  layout Boring do
-    message("Happy","hungry")
-    mess
-    message("Angry","thirsty")
+ 
+  layout Haha do
+    message("Happy ","hungry")
+    message("Mad ","thirsty")
+    message("Bored ","fully")
   end
 end
-
-
-
+  
+  # expr = 
+  # quote do 
+  #   layout Haha do
+  #     message("Happy ","hungry")
+  #     message("Mad ","thirsty")
+  #     message("Bored ","fully")
+  #   end
+  # end
+  # res = Macro.expand_once(expr,__ENV__)
+  # IO.puts(Macro.to_string(res))
 
 
 # 函数调用宏
