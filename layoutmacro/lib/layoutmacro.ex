7d36d966ba576209788f0b24633a46c6e6df1607
Shuzhiyuan <shuzhiyuan02@163.com>
2021-04-21 13:59:04 +0800

    next step should vertify the message direct invoke

--- layoutmacro/lib/layoutmacro.ex
+++ layoutmacro/lib/layoutmacro.ex
@@ -3,39 +3,40 @@
 defmodule Layoutmacro do
   defmacro layout(module_name,do: clause) do
     {:__block__,[],message_list} = clause
+
+    # 得到message的信息，
     messages = Enum.map(message_list,fn message ->
       {_,_,[emotion,status]} = message
       {emotion,status}
     end)
 
-    funcs = Enum.map(messages,fn message -> quote do assert message(unquote(message)) end end)
-
-    quote do
+    funcs = Enum.map(messages,fn message -> quote do assert func_message(unquote(message)) end end)
+    [fun1,fun2,fun3] = funcs
+    quote do 
       ExUnit.start()
       defmodule unquote(module_name) do
         use ExUnit.Case, async: true
-        unquote (funcs)
+        unquote (fun1)
+        unquote (fun2)
+        unquote (fun3)
+        #unquote(funcs)
       end
-    end
+    end  
+
   end
 
 
-  def message({emotion,status}) do
+  def func_message({emotion,status}) do
     IO.inspect(emotion<>status)
+    IO.inspect("DayDayUp")
   end
 
 end
 
 defmodule Run do
   import Layoutmacro
- 
-  layout Haha do
-    message("Happy ","hungry")
-    message("Mad ","thirsty")
-    message("Bored ","fully")
-  end
-end
-  
+   
+# 查看扩展函数结果
   # expr = 
   # quote do 
   #   layout Haha do
@@ -46,6 +47,14 @@ end
   # end
   # res = Macro.expand_once(expr,__ENV__)
   # IO.puts(Macro.to_string(res))
+
+  layout Haha do
+    message("Happy ","hungry")
+    message("Mad ","thirsty")
+    message("Bored ","fully")
+  end
+end
+ 
 
 
 # 函数调用宏
---
+++ layoutmacro/lib/listrelace.ex
@@ -0,0 +1,14 @@
+defmodule ListRelase do
+  defmacro list(get_list)do
+    
+  end
+  
+  defmacro relase_list do
+    
+  end 
+
+end 
+
+
+
+# 必须尽快完成    任务
