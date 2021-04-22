6b82acbd52c3124d9cc62b911a1479d95dd54fdb
Shuzhiyuan <shuzhiyuan02@163.com>
2021-04-22 17:49:21 +0800

    add rebuild file

---
+++ rebuild/lib/rebuild.ex
@@ -0,0 +1,18 @@
+defmodule Rebuild do
+  @moduledoc """
+  Documentation for `Rebuild`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rebuild.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ rebuild/test/rebuild_test.exs
@@ -0,0 +1,8 @@
+defmodule RebuildTest do
+  use ExUnit.Case
+  doctest Rebuild
+
+  test "greets the world" do
+    assert Rebuild.hello() == :world
+  end
+end
