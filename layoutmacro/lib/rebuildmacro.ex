061d4e4c0cd8ffff7779d2376faa8716d19360e4
Shuzhiyuan <shuzhiyuan02@163.com>
2021-04-22 09:08:35 +0800

    rebuild macro demo

--- layoutmacro/lib/rebuildmacro.ex
+++ layoutmacro/lib/rebuildmacro.ex
@@ -1,17 +1,13 @@
 # ExUnit.start()
 # use ExUnit.Case, async: true
-defmodule Layoutmacro do
+defmodule Rebuildmacro do
   defmacro layout(module_name,do: clause) do
     {:__block__,[],message_list} = clause
 
     # 得到message的信息，
-    messages = Enum.map(message_list,fn message ->
-      {_,_,[emotion,status]} = message
-      {emotion,status}
-    end)
-
-    funcs = Enum.map(messages,fn message -> quote do assert func_message(unquote(message)) end end)
-    [fun1,fun2,fun3] = funcs
+    messages = Enum.map(message_list,fn message -> get_argue(message)end)
+    funcs    = Enum.map(messages,    fn message -> compose_arguement(message) end)
+    [fun1,fun2,fun3] = funcs  #push from list 
     quote do 
       ExUnit.start()
       defmodule unquote(module_name) do
@@ -19,34 +15,34 @@ defmodule Layoutmacro do
         unquote (fun1)
         unquote (fun2)
         unquote (fun3)
-        #unquote(funcs)
       end
     end  
-
   end
 
+  defp compose_arguement(message)do
+    quote do 
+      assert func_message(unquote(message)) 
+    end
+  end 
 
   def func_message({emotion,status}) do
+    # 这种形式具备更好的扩展性
     IO.inspect(emotion<>status)
     IO.inspect("DayDayUp")
   end
+  def get_argue(message) do
+    {_,_,[emotion,status]} = message
+    {emotion,status}
+  end 
 
+  def showAST(expr) do
+    res = Macro.expand_once(expr,__ENV__)
+    IO.puts(Macro.to_string(res))
+  end
 end
 
 defmodule Run do
   import Layoutmacro
-   
-# 查看扩展函数结果
-  # expr = 
-  # quote do 
-  #   layout Haha do
-  #     message("Happy ","hungry")
-  #     message("Mad ","thirsty")
-  #     message("Bored ","fully")
-  #   end
-  # end
-  # res = Macro.expand_once(expr,__ENV__)
-  # IO.puts(Macro.to_string(res))
 
   layout Haha do
     message("Happy ","hungry")
@@ -56,5 +52,3 @@ defmodule Run do
 end
  
 
-
-# 函数调用宏
