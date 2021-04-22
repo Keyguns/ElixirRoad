4339b8db86dac9c3630b7b23b2d41f04cd2acda2
Shuzhiyuan <shuzhiyuan02@163.com>
2021-04-22 15:49:45 +0800

    the best way use func_name to invoke func is using maco

--- layoutmacro/lib/macro_coat.exs
+++ layoutmacro/lib/macro_coat.exs
@@ -11,20 +11,28 @@ defmodule Coat do
   end
 
 ### macro version
-
   defmacro message(emotion,status)do
     IO.inspect("B")
     quote do
       IO.inspect(unquote(emotion)<>unquote(status))
     end
   end 
-end
+
+  defmacro invoke_func({func_name,_,_},actor)do
+    {func_name,[],[actor]}
+  end
+
 
 ### func version 
-defmodule FuncLib do
   def message_fun(emotion,status) do
     IO.inspect(emotion<>status)
   end
+end
+
+
+defmodule FuncLib do
+  def func_1(actor),do: IO.inspect(actor,label: "func_1")
+  def func_2(actor),do: IO.inspect(actor,label: "func_2")
 end
 
 defmodule Run do
@@ -35,5 +43,7 @@ defmodule Run do
     message_fun("happy","hungry")
     message_fun("angry","thirsty")
     message_fun("sad","hungry and thirsty")
+    invoke_func(func_1,"Ben")          # 搞定了
+    invoke_func(func_2,"LaDen")
   end
 end
